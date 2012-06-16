b0252b36b668afff34d5589672c182648fb85387
Igor Lifar <igor.lifar@omnihead.ru>
2012-06-16 20:17:21 +0400

    new text and js

--- server/pages/ajax/urls.py
+++ server/pages/ajax/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('pages.ajax.handlers',
 	url(r'^get_page_part/', 'get_page_part'),
+	url(r'^feedback/', 'feedback')
 )
--- templates/page.js
+++ templates/page.js
@@ -1,1 +1,13 @@
-write your js here
+$(document).ready(function() {
+   
+    $('a.scroll-top').click(function(){
+    	console.log("test")
+        $('html, body').animate({scrollTop:0}, 'slow');
+        return false;
+    });
+
+	$(".scroll").click(function(event){		
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+	});
+});
